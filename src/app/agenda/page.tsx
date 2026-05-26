761fc1bcfe294cd645e706966a478d324cc28a41
matiaspegasano <matias.pegasano@gmail.com>
2026-05-25 23:35:19 -0300

    auto: src/app/agenda/page.tsx,

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import Link from 'next/link';
 import TitleBar from '@/components/TitleBar';
 import Footer from '@/components/Footer';
